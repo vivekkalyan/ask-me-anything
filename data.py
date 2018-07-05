383d01c3e61774955e7e87ee8746bf128256964e
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-07-05 20:33:22 +0800

    Create COCO images dataset class

---
+++ data.py
@@ -0,0 +1,35 @@
+import os
+
+import torch.utils.data as data
+from PIL import Image
+
+class CocoImages(data.Dataset):
+    def __init__(self, path, transform=None):
+        super(CocoImages, self).__init__()
+        self.path = path
+        self.id_to_filename = self._crawl_images()
+        self.sorted_ids = sorted(self.id_to_filename.keys())  # used for deterministic iteration order
+        print('found {} images in {}'.format(len(self), self.path))
+        self.transform = transform
+
+    def _crawl_images(self):
+        id_to_filename = {}
+        for filename in os.listdir(self.path):
+            if not filename.endswith('.jpg'):
+                continue
+            id_and_extension = filename.split('_')[-1]
+            id = int(id_and_extension.split('.')[0])
+            id_to_filename[id] = filename
+        return id_to_filename
+
+    def __getitem__(self, item):
+        id = self.sorted_ids[item]
+        path = os.path.join(self.path, self.id_to_filename[id])
+        img = Image.open(path).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+        return id, img
+
+    def __len__(self):
+        return len(self.sorted_ids)
