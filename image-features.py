b0048ae65431c4d5cc40795b5b4151e8c2a28639
Vivek Kalyan <vivekkalyansk@gmail.com>
2018-07-05 21:50:05 +0800

    Add transforms and data loader

--- image-features.py
+++ image-features.py
@@ -1,5 +1,8 @@
 import torch
+import torch.nn as nn
 from torchvision import models
+
+import config
 
 class ImageFeaturesNet(nn.Module):
     def __init__(self):
@@ -14,3 +17,32 @@ class ImageFeaturesNet(nn.Module):
     def forward(self, x):
         self.model(x)
         return self.buffer
+
+def get_transform(target_size, scale_fraction=1.0):
+    return transforms.Compose([
+        transforms.Resize(int(target_size / scale_fraction)),
+        transforms.CenterCrop(target_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ])
+
+def create_coco_loader(path):
+    transform = get_transform(config.image_size, config.scale_fraction)
+    data_loader = torch.utils.data.DataLoader(
+        data.CocoImages(path, transform=transform),
+        batch_size=config.image_batch_size,
+        num_workers=config.data_workers,
+        shuffle=False,
+        pin_memory=True,
+    )
+    return data_loader
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net = ImageFeaturesNet()
+    net.eval()
+    net.to(device)
+
+    train_loader = create_coco_loader(config.train_path)
+    val_loader = create_coco_loader(config.val_path)
