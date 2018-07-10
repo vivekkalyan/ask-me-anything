cc078caf50853a2db64434b320133b34666eddf8
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-07-10 08:56:24 +0800

    Faster punc_handler & more detailed logging in vocab

--- vocab.py
+++ vocab.py
@@ -5,36 +5,51 @@ import config
 from os.path import isfile
 import re
 
+from tqdm import tqdm
+
 
 def get_all_questions(questions_list):
     # remove the question mark at the end and make everything lowercase
     # return a list of list of words that correspond to question in question lists
-    return [q["question"].lower()[:-1].split(' ') for q in questions_list]
+    rtv = []
+    for q in tqdm(questions_list):
+        rtv.append(q["question"].lower()[:-1].split(' '))
+    return rtv
 
 
-def _handle_punctuation(s):
+def _handle_punctuation():
     punctuation_chars = re.escape(r';/[]"{}()=+\_-><@`,?!')
     punctuation = re.compile(r'([{}])'.format(re.escape(punctuation_chars)))
     punctuation_with_space = re.compile(
         r'(?<= )([{0}])|([{0}])(?= )'.format(punctuation_chars))
+    comma = re.compile(r'(\d)(,)(\d)')
+    period = re.compile(r'(?!<=\d)(\.)(?!\d)')
 
-    if punctuation.search(s) is not None:
-        s = punctuation_with_space.sub('', s)
-        if re.search(re.compile(r'(\d)(,)(\d)'), s) is not None:
-            s = s.replace(',', '')
-        s = punctuation.sub(' ', s)
-        s = re.compile(r'(?!<=\d)(\.)(?!\d)').sub('', s)
+    def handle(s):
+        if punctuation.search(s) is not None:
+            s = punctuation_with_space.sub('', s)
+            if re.search(comma, s) is not None:
+                s = s.replace(',', '')
+            s = punctuation.sub(' ', s)
+            s = period.sub('', s)
 
-    return s
+        return s
+
+    return handle
 
 
 def get_all_answers(annotations):
-    list_of_list_of_answers = [
-        [a['answer'] for a in ans_dict['answers']] for ans_dict in annotations]
+    print('Extracting from annotations json ...')
+    list_of_list_of_answers = []
+    for ans_dict in tqdm(annotations):
+        list_of_list_of_answers.append([a['answer'] for a in ans_dict['answers']])
 
-    for i, list_of_answers in enumerate(list_of_list_of_answers):
+    handle_punctuation_fn = _handle_punctuation()
+    print('Cleaning answers\'s punctuations...')
+    for i in tqdm(range(len(list_of_list_of_answers))):
+        list_of_answers = list_of_list_of_answers[i]
         list_of_list_of_answers[i] = list(
-            map(_handle_punctuation, list_of_answers))
+            map(handle_punctuation_fn, list_of_answers))
 
     return list_of_list_of_answers
 
