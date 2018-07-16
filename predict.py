49da8640c8be72ffd7469acc51135f20433dc45b
Stanley Nguyen <hung.ngn.the@gmail.com>
2018-07-16 13:08:32 +0800

    Model to eval mode during prediction

--- predict.py
+++ predict.py
@@ -7,14 +7,19 @@ import config
 import image_features
 import pdb
 
-q_vocab, a_vocab = vocab.retrieve_vocab(config.vocab_questions_path, config.vocab_answers_path, config.questions_train_path, config.annotations_train_path)
+q_vocab, a_vocab = vocab.retrieve_vocab(
+    config.vocab_questions_path, config.vocab_answers_path, config.questions_train_path, config.annotations_train_path)
 imageNet = image_features.ImageFeaturesNet()
+imageNet.eval()
 mainModel = model.MainModel(len(q_vocab)+1)
+mainModel.eval()
 checkpoint = torch.load('weights', map_location='cpu')
 mainModel.load_state_dict(checkpoint['state_dict'])
-transform = image_features.get_transform(config.image_size, config.scale_fraction)
+transform = image_features.get_transform(
+    config.image_size, config.scale_fraction)
 
-def predict(img_path, qn): 
+
+def predict(img_path, qn):
     img = Image.open(img_path).convert('RGB')
     img = transform(img)
     img.unsqueeze_(0)  # add batch size dim of 1
@@ -28,10 +33,12 @@ def predict(img_path, qn):
     out = F.softmax(out[0], 0)
     topk_prob, topk_idx = torch.topk(out, 5)
 
-    topk_ans = [next(k for k, v in a_vocab.items() if v == idx) for idx in topk_idx]
+    topk_ans = [next(k for k, v in a_vocab.items() if v == idx)
+                for idx in topk_idx]
     predictions = [[topk_ans[i], topk_prob[i].item()] for i in range(5)]
     print(predictions)
     return predictions
+
 
 def process_question(question, q_vocab):
     rtv = torch.zeros(len(question)).long()
@@ -42,5 +49,7 @@ def process_question(question, q_vocab):
 
     return rtv, len(question)
 
+
 if __name__ == '__main__':
-    predict('./data/val2014/COCO_val2014_000000000328.jpg', 'how many people are there')
+    predict('./data/val2014/COCO_val2014_000000000328.jpg',
+            'how many people are there')
